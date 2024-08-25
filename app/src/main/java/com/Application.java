bd188de4d8349b12a906c1ff4ea15da88f0d0834
Ciamcio <jakub.maciocha@gmail.com>
2024-08-25 19:32:23 +0200

    Updating the code with the full version of the project

---
+++ app/src/main/java/com/Application.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Application {
+    public Application() {
+        new RoadPanel();
+    }
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() ->  {
+            new Application();
+        });
+    }
+}
